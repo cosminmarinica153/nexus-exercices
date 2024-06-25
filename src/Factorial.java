6ebb5206109dd1f51fca036329080f441e22f668
ZxPower <zxpower@yahoo.com>
2024-06-25 19:00:52 +0300

    initial commit

---
+++ src/Factorial.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public long calculate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type in the number you would like to find the factorial of: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+            return -1;
+        } else {
+            return recursiveFactorial(number);
+        }
+    }
+
+    public long recursiveFactorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * recursiveFactorial(num - 1);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+//         Factorial
+        Factorial factorial = new Factorial();
+        System.out.println(factorial.calculate());
+//         Prime
+        Prime prime = new Prime();
+        System.out.println(prime.check());
+    }
+}
