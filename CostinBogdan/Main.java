6d159c22d6bc1886d65f34916bf8d8a6b36839eb
ZxPower <zxpower@yahoo.com>
2024-06-25 19:43:28 +0300

    initial commit

---
+++ CostinBogdan/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+////         Factorial
+//        Factorial factorial = new Factorial();
+//        System.out.println(factorial.calculate());
+////         Prime
+//        Prime prime = new Prime();
+//        System.out.println(prime.check());
+////        Fibonacci
+//        Fibonacci fibonacci = new Fibonacci();
+//        System.out.println(fibonacci.check());
+////        CheckPalindrome
+//        Palindrome palindrome = new Palindrome();
+//        System.out.println(palindrome.check());
+    }
+}
