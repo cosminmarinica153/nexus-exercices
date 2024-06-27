177187165f641601e88a557bccf7a99c3a47ee4e
ZxPower145 <119930303+ZxPower145@users.noreply.github.com>
2024-06-28 00:34:34 +0300

    Delete CostinBogdan directory

--- CostinBogdan/Contest_2_3/Contest3.java
+++
@@ -1,9 +0,0 @@
-package Contest_2_3;
-
-public class Contest3 {
-    public static void main(String[] args) {
-        BullsAndCowsUtil util = new BullsAndCowsUtil();
-        util.playInt();
-//        util.playString();
-    }
-}
